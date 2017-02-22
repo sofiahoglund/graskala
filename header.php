ab21f53baa76d55e2eceb69e02ff3299efe39523
sofiahoglund <sofho329@student.liu.se>
2017-02-22 15:39:05 +0100

    header

--- header.php
+++ header.php
@@ -23,16 +23,13 @@
 
 	<div class="blog-masthead">
 		<div class="container">
-			<nav class="blog-nav">
-				<a class="blog-nav-item active" href="#">Home</a>
-				<?php wp_list_pages( '&title_li=' ); ?>
-			</nav>
+			<div class="blog-header">
+			<h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
+			</div>
+			
 		</div>
 	</div>
 	
 	<div class="container">
 
-		<div class="blog-header">
-			<h1 class="blog-title"><a href="<?php bloginfo( 'wpurl' );?>"><?php echo get_bloginfo( 'name' ); ?></a></h1>
-			<p class="lead blog-description"><?php echo get_bloginfo( 'description' ); ?></p>
-		</div>
+		
